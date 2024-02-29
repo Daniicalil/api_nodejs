ffb90940faee6cea2232554b8add7ad250e1b69c
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-02-29 20:35:27 -0300

    Criação da rota inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+//const porta = process.env.PORT || 3333;
+const porta = 3333;
+
+app.listen(porta, () => {
+    console.log('Servidor iniciado na porta' + porta);
+    //console.log(`Servidor iniciado na porta ${porta}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Hello World');
+});
+
+// request: informações vindo do front-end
+// response: informações mandando para o front-end
