3f8569e78e9fb38951904168af5f1bebdaf49f6c
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-03-14 20:40:20 -0300

    Aula 14-03

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,12 +1,20 @@
 const { json } = require('express');
-const db = require ('../database/connection')/
+const db = require ('../database/connection');
 
 module.exports = {
-    async listarMesas(request, response) {
+    async listarUsuarios(request, response) {           //request: vem do frontend e response: vai para o frontend
         try {
-            return response.status(200).json({confirma: 'Listar Mesas'});
+            return response.status(200).json({
+                sucesso: true,                          //sucesso:chave e true:valor
+                mensagem: 'Lista de usuários.',
+                dados: null
+            });
         } catch (error) {
-            return response.status(500).json({confirma: 'Erro', message: error});
-        },
-    };
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: `Erro na requisição. \n ${error}`,
+                dados: null
+            });
+        }
+    },
 }
