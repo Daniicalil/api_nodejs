7afc3cf7a8f40e4f64d54cd6bed736314d1f0a28
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-04-09 20:37:12 -0300

    Aula 09-04

--- controllers/ingredientes.js
+++ controllers/ingredientes.js
@@ -4,14 +4,15 @@ const db = require('../database/connection');
 module.exports = {
     async listarIngredientes(request, response) {
         try {
-            // instruções SQL
+            const {ing_nome} = request.body;
+            const ingPesq = `%${ing_nome}%`
             const sql = `SELECT 
-                ing_id, ing_nome, ing_img, ing_custo_adicional
+                ing_id, ing_nome, ing_img, ing_custo_adicional 
                 FROM Ingredientes 
-                WHERE ing_nome like '%${ing_nome}%';`;
-            // executa instruções SQL e armazena o resultado na variável usuários
+                WHERE ing_nome like ?;`;
+
+            const values = [ingPesq];
             const ingredientes = await db.query(sql, values);
-            // armazena em uma variável o número de registros retornados
             const nItens = ingredientes[0].length;
 
             return response.status(200).json({
@@ -30,24 +31,18 @@ module.exports = {
     },
     async cadastrarIngredientes(request, response) {
         try {
-            // parâmetros recebidos no corpo da requisição
             const { ing_nome, ing_img, ing_custo_adicional } = request.body;
-            // instrução SQL
             const sql = `INSERT INTO ingredientes
-                (ing_id, ing_nome, ing_img, ing_custo_adicional) 
-                VALUES (?, ?, ?, ?)`;
-            // definição dos dados a serem inseridos em um array
+                (ing_nome, ing_img, ing_custo_adicional) 
+                VALUES (?, ?, ?)`;
             const values = [ing_nome, ing_img, ing_custo_adicional];
-            // execução da instrução sql passando os parâmetros
             const execSql = await db.query(sql, values);
-            // identificação do ID do registro inserido
             const ing_id = execSql[0].insertId;
 
             return response.status(200).json({
                 sucesso: true,
                 mensagem: 'Cadastro de ingrediente efetuado com sucesso.',
                 dados: ing_id
-                //mensSql: execSql
             });
         } catch (error) {
             return response.status(500).json({
@@ -59,16 +54,12 @@ module.exports = {
     },
     async editarIngredientes(request, response) {
         try {
-            // parâmetros recebidos pelo corpo da requisição
             const { ing_nome, ing_img, ing_custo_adicional } = request.body;
             // parâmetro recebido pela URL via params ex: /usuario/1
             const { ing_id } = request.params;
-            // instruções SQL
             const sql = `UPDATE ingredientes SET ing_nome = ?, ing_img = ?, 
                 ing_custo_adicional = ? WHERE ing_id = ?;`;
-            // preparo do array com dados que serão atualizados
             const values = [ing_nome, ing_img, ing_custo_adicional, ing_id];
-            // execução e obtenção de confirmação da atualização realizada
             const atualizaDados = await db.query(sql, values);
 
             return response.status(200).json({
